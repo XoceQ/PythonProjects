3d31fef031dbd43c9503cf9bf99115ca43e63fa6
XoceQ <73414406+XoceQ@users.noreply.github.com>
2022-02-21 09:45:52 -0500

    Add files via upload

---
+++ XoceQRcode.py
@@ -0,0 +1,46 @@
+# import modules
+import qrcode
+from PIL import Image
+
+# taking image which user wants
+# in the QR code center
+Logo_link = 'logoXoce.jpg'
+
+logo = Image.open(Logo_link)
+
+# taking base width
+basewidth = 100
+
+# adjust image size
+wpercent = (basewidth/float(logo.size[0]))
+hsize = int((float(logo.size[1])*float(wpercent)))
+logo = logo.resize((basewidth, hsize), Image.ANTIALIAS)
+QRcode = qrcode.QRCode(
+	error_correction=qrcode.constants.ERROR_CORRECT_H
+)
+
+# taking url or text
+url = 'https://github.com/XoceQ'
+
+# adding URL or text to QRcode
+QRcode.add_data(url)
+
+# generating QR code
+QRcode.make()
+
+# taking color name from user
+QRcolor = 'Red'
+
+# adding color to QR code
+QRimg = QRcode.make_image(
+	fill_color=QRcolor, back_color="white").convert('RGB')
+
+# set size of QR code
+pos = ((QRimg.size[0] - logo.size[0]) // 2,
+	(QRimg.size[1] - logo.size[1]) // 2)
+QRimg.paste(logo, pos)
+
+# save the QR code generated
+QRimg.save('Xoce_QR.png')
+
+print('QR code generated!')
